fbe5e3e2bfb447c87ebef07b040f14a920a6abee
Vitor.Castro <vitor.oliveira@digitalscutum.com.br>
2023-06-13 16:07:26 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primeiro-app';
+  numero : number = 1;
+
+
+  aumentar(){
+    this.numero ++;
+  }
+
+
+  diminuir(){
+    this.numero --;
+  }
+
+
+  repete(n: number): Array<number> {
+    return Array(n);
+  }
+
+
 }
